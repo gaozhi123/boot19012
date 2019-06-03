01d14a6d4c2dd139d4662770792544dd313d326a
gaozhi <gaozhi_1142629468@qq.com>
2019-06-03 17:00:38 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/boot1901/Boot1901Application.java
@@ -0,0 +1,13 @@
+package com.qfedu.boot1901;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot1901Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot1901Application.class, args);
+    }
+
+}
